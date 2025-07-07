4ae55e3ec78e482c97019a899eb982c916ec51a8
Debobrata Modak <91485525+debobrata565@users.noreply.github.com>
2025-07-07 19:21:01 +0000

    operation

---
+++ javascript/operation.js
@@ -0,0 +1,19 @@
+let value = 10
+let negValue = - value
+// console.log(negValue) // output will be -10
+
+console.log(2**3) // it means 2^3 in js
+
+let str1= "Hello "
+let str2= "Debu"
+let str3= str1 + str2
+console.log(str3)
+
+console.log("1" + 2) // ouput 12
+console.log(1 + "2") // this ouput also 12
+console.log("1" + 2 + 2) // output will be 122
+console.log(1 + 2 + "2") // output will be 32
+
+let gameCounter = 100
+++ gameCounter;
+console.log(gameCounter)// ouput will be 101
