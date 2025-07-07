fc72436709ff801c07524e8af610df1b5b918f69
Debobrata Modak <91485525+debobrata565@users.noreply.github.com>
2025-07-07 18:02:17 +0000

    datatypes done

---
+++ javascript/dataTypes.js
@@ -0,0 +1,24 @@
+// alert(3+3) // it will not work as we are using nodejs not browser
+
+let name ="Debu"
+let age = 24
+let isLoggedIn = true
+
+console.table([name, age, isLoggedIn])
+
+/* types of datatypes in js
+// number
+// string = ""
+// boolean
+// bigint = true/false
+// null = standalone value
+// undefined = value not assingned
+// symbol = unique
+
+// object
+*/
+
+console.log(typeof true)
+
+console.log(typeof null)// null type defined as a object in js
+console.log(typeof undefined) // undfined type will be undefined
