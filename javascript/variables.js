69939d60968ae9e0bfb66068c0fa366652c3992e
Debobrata Modak <91485525+debobrata565@users.noreply.github.com>
2025-07-07 17:22:15 +0000

    variable done correctly

--- javascript/variables.js
+++ javascript/variables.js
@@ -2,6 +2,7 @@ const userId = 46365750
 let userName = "debo34@gmail.com"
 var passWord = "Deb@098"
 userCity = "Kolkata"
+let userState
 
 // userId = 46365790 // not allowed as userId is already mentioned as constant 
 
@@ -10,5 +11,8 @@ console.log(userId)
 /* prefer not to use var in js
 because of issue in block scope and functional scope*/
 
+/* in js if you not declare anything
+in variable it will take as undefined in js*/
 
-console.table([userId, userName, passWord, userCity])
+
+console.table([userId, userName, passWord, userCity, userState])
