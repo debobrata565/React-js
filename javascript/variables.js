9ea9332ba91c7062f07111f3311652072b59a9f8
Debobrata Modak <91485525+debobrata565@users.noreply.github.com>
2025-07-07 17:13:38 +0000

    variable done

---
+++ javascript/variables.js
@@ -0,0 +1,14 @@
+const userId = 46365750
+let userName = "debo34@gmail.com"
+var passWord = "Deb@098"
+userCity = "Kolkata"
+
+// userId = 46365790 // not allowed as userId is already mentioned as constant 
+
+console.log(userId)
+
+/* prefer not to use var in js
+because of issue in block scope and functional scope*/
+
+
+console.table([userId, userName, passWord, userCity])
